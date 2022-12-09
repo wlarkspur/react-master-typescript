247cd6227158681ae234c0706641f29463f45e41
Seokmin <newacon@naver.com>
2022-12-09 22:26:49 +0200

    react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyProps) {
-  return <H1>{text}</H1>;
-}
+import Router from "./Router";
 
 function App() {
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
-  return (
-    <Container>
-      <H1>protected!</H1>
-      <Dummy active text="hello" />
-
-      <button onClick={onClick}>click me</button>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Coin />}></Route>
+        <Route path="/:coinId" element={<Coins />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
