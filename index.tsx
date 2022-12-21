f67cb3e0b57bddab3da2a87ca07d4d111e764201
wlarkspur <newacon@naver.com>
2022-12-21 10:50:17 +0200

    index.tsx ->root

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
-import router from "./Router";
+import router from "./src/Router";
 import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
